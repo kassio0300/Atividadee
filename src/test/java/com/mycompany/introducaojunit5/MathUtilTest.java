7a5cf621275b01ef5094bb33d0eda3cdcf79a386
kassi <kassi@LAPTOP-1T4SBFAN>
2021-06-04 17:39:11 -0300

    teste

--- src/test/java/com/mycompany/introducaojunit5/MathUtilTest.java
+++ src/test/java/com/mycompany/introducaojunit5/MathUtilTest.java
@@ -3,6 +3,7 @@ package com.mycompany.introducaojunit5;
 
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
+import static com.mycompany.introducaojunit5.MathUtil.mdc;
 
 
 public class MathUtilTest {
@@ -25,5 +26,105 @@ public class MathUtilTest {
         assertEquals(esperado, obtido);
         
     }
+    
+    @Test
+    void testMdcParesP2(){
+        final int a = 16;
+        final int b = 8;
+        final int divisor = 4;
+        final int esperado = 0;
+        final int obtido = MathUtil.mdc(a,b);
+        //assertEquals(esperado, obtido % divisor);
+        assertTrue(obtido % divisor == 0);
+    }
+    
+    @Test
+    void testMdcAPositivoP3(){
+            final int a = 15;
+            final int b = 0;
+            final int esperado = 15;
+            final int obtido = MathUtil.mdc(a, b);
+            assertEquals(esperado, obtido);
+    
+    }
+    
+    @Test
+    void testMdcANegativoP3(){
+            final int a = -30;
+            final int b = 0;
+            final int esperado = 30;
+            final int obtido = MathUtil.mdc(a, b);
+            assertEquals(esperado, obtido);
+    
+    }
+    
+    @Test
+    void testMdcMParP4(){
+            final int a = 9;
+            final int b = 3;
+            final int m = 2;
+            final int esperado = MathUtil.mdc(a*m, b*m);
+            final int obtido = m * MathUtil.mdc(a, b);
+            assertEquals(esperado, obtido);
+    
+    }
+    
+     @Test
+     void testMdcP5(){
+            final int a = 7;
+            final int b = 3;
+            final int c = 5;
+            final int esperado = mdc(a*b, c);
+            final int obtido = mdc(b, c);
+            System.out.println("Esperado: " + esperado + "Obtido: " + obtido);
+            assertEquals(esperado, obtido);
+    
+    }
+    
+     @Test
+     void testMdcP5Resultado1(){
+            final int a = 7;
+            final int b = 3;
+            final int c = 5;
+            final int esperado = 1;
+            final int obtido = mdc(b, c);
+            System.out.println("Esperado: " + esperado + "Obtido: " + obtido);
+            assertEquals(esperado, obtido);
+    
+    }
+    
+     @Test
+     void testMdcP6(){
+            final int a = 8;
+            final int b = 2;
+            final int esperado = mdc(a,b);
+            final int obtido = mdc(b, a);
+            assertEquals(esperado, obtido);
+    
+    }
+     
+        @Test
+        void testMdcP7TudoNegativo(){
+            final int a = -12;
+            final int b = -6;
+            final int esperado = 6;
+            final int obtido = mdc(a, b);
+            assertEquals(esperado, obtido);
+    
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
     
 }
